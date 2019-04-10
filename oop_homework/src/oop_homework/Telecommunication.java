75b9f2bb8a340a936a72fa7d79b717f1dca5a5a1
robydias <rarabsaid@gmail.com>
2019-04-10 17:57:40 -0400

    first commit

---
+++ oop_homework/src/oop_homework/Telecommunication.java
@@ -0,0 +1,8 @@
+package oop_homework;
+
+public interface Telecommunication {
+public abstract boolean gettransmitter();
+public abstract void settransmitter(boolean n );
+ public abstract  boolean getreceiver();
+ public abstract void setreciever (boolean m);
+}
---
+++ oop_homework/src/oop_homework/Vediocall.java
@@ -0,0 +1,14 @@
+package oop_homework;
+
+public class Vediocall implements Telecommunication{
+
+public boolean gettransmitter() {
+	getrecievert
+}
+public void settransmitter(boolean n );
+ public   boolean getreceiver();
+ public  void setreciever (boolean m=true) {
+	 system.out.println
+ }
+
+}
